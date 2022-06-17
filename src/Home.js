a8f9048db731752b48801d97a7a28feb2d7f9871
aminmallol <aminm-34@hotmail.fr>
2022-06-17 13:53:28 +0100

    Changed some html

--- src/Home.js
+++ src/Home.js
@@ -2,15 +2,17 @@ import { useState } from "react"
 
 const Home = () => {
   const [name, setName] = useState("Mario")
+  const [age, setAge] = useState(25)
 
   const handleClick = (e) => {
     setName("Luigi")
+    setAge(30)
   }
 
   return (
     <div className="home">
       <h2>Homepage</h2>
-      <p>{ name }</p>
+      <p>{ name } is { age } years old</p>
       <button onClick={handleClick}>Click me</button>
     </div>
   );
