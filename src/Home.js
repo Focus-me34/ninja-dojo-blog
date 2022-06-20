d2b32c80d442bc5f5231648bb7881f2c09916c56
aminmallol <aminm-34@hotmail.fr>
2022-06-20 16:41:40 +0100

    End of video number 15: Using the useEffect hook with dependency array

--- src/Home.js
+++ src/Home.js
@@ -1,19 +1,31 @@
-import { useState } from "react"
+import { useEffect, useState } from "react"
+import BlogList from "./BlogList";
+
 
 const Home = () => {
-  const [name, setName] = useState("Mario")
-  const [age, setAge] = useState(25)
+  const [blogs, setBlogs] = useState([
+    { title: 'My new website', body: 'lorem ipsum...', author: 'mario', id: 1 },
+    { title: 'Welcome party!', body: 'lorem ipsum...', author: 'yoshi', id: 2 },
+    { title: 'Web dev top tips', body: 'lorem ipsum...', author: 'mario', id: 3 }
+  ])
 
-  const handleClick = (e) => {
-    setName("Luigi")
-    setAge(30)
+  const [name, setName] = useState("Mario")
+
+  const handleDelete = (id) => {
+    const newBlogs = blogs.filter(blog => blog.id !== id)
+    setBlogs(newBlogs)
   }
+
+  useEffect(() => {
+    console.log("useEffect ran");
+  },[name])
 
   return (
     <div className="home">
-      <h2>Homepage</h2>
-      <p>{ name } is { age } years old</p>
-      <button onClick={handleClick}>Click me</button>
+      <BlogList blogs={blogs} title="All blogs!" handleDelete={handleDelete} />
+      <button onClick={() => setName("Luigi")}>Change name</button>
+      <p>{name}</p>
+      {/* <BlogList  blogs={blogs.filter((item) => item.author == "mario" )} title="Mario's blogs"/> */}
     </div>
   );
 }
