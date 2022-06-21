356bf46d3a5a73355c3c1bcb8120218bfeb681ff
aminmallol <aminm-34@hotmail.fr>
2022-06-21 14:39:14 +0100

    Finished lecture 22: React router - Exact route matching

--- src/App.js
+++ src/App.js
@@ -1,15 +1,27 @@
-import "./index.css";
 import Navbar from './Navbar';
 import Home from './Home';
+import Create from './Create';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import "./index.css";
 
 function App() {
   return (
-    <div className="App">
-      <Navbar />
-      <div className='content'>
-        <Home />
+    <Router> // ! Router functionality starts here
+      <div className="App">
+        <Navbar />
+        <div className='content'>
+          <Switch> // ! Switch begin here
+            <Route exact path="/">       // ! Route 1: root_path
+              <Home />
+            </Route>
+            <Route exact path="/create"> // ! Route 2: "/create"
+              <Create />
+            </Route>
+          </Switch> // ! Switch ends here
+        </div>
       </div>
-    </div>
+
+    </Router> // ! Router functionality stops here
   );
 }
 
